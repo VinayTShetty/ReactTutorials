657d81e7b10703180218ebaa658b77f7397a21c5
Vinay T S <vinaytshetty@gmail.com>
2021-08-27 11:12:51 +0530

    Using Bold Text and Padding and Background Color change in the applicaiton.

--- ReactTutorials/App.js
+++ ReactTutorials/App.js
@@ -5,8 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+      <Text style={styles.boldText}> Hello World</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'pink',
+    padding:20
+  }
 });
