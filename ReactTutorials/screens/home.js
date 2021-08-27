6e8f868b47cf910823be1b124ca19df23aa9841f
Vinay T S <vinaytshetty@gmail.com>
2021-08-27 19:14:47 +0530

    Showing the Flat list components in the Home screen is completed.

--- ReactTutorials/screens/home.js
+++ ReactTutorials/screens/home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Text,Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList,Image }from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home(props) {
@@ -8,14 +8,33 @@ const pressHandler=()=>{
   props.navigation.navigate('ReviewDetails')
 }
 
-  return (
-    <View style={globalStyles.container}>
-      <Text>Home Demo Screen</Text>
-      <Button title ='Go to Review Details ' onPress={
-        pressHandler
-      }/>
-    </View>
-  );
+
+const IMAGES = {
+  image1: require('../assets/adaptive-icon.png'),
+  image2: require('../assets/adaptive-icon.png'),
+  image3: require('../assets/adaptive-icon.png'),
+  image4: require('../assets/adaptive-icon.png'),
+  image5: require('../assets/adaptive-icon.png'),
+  image6: require('../assets/adaptive-icon.png'),
+};
+
+const [reviews, setReviews] = useState([
+  { title: 'Empire Restaurant', menu: "Pizza", body: 'lorem ipsum',image:IMAGES.image1 ,key: '1' },
+  { title: 'King Restaurant', menu: "Meals", body: 'lorem ipsum', image:IMAGES.image2 ,key: '2' },
+  { title: 'ABC Restaurant', menu: "PotatoFries", body: 'lorem ipsum',image:IMAGES.image3 , key: '3' },
+  { title: 'Ginger Restaurant', menu: "Sandwich", body: 'lorem ipsum', image:IMAGES.image4 ,key: '4' },
+  { title: 'Tasty Restuarant', menu: "Chicken", body: 'lorem ipsum', image:IMAGES.image5 ,key: '5' },
+]);
+
+return (
+  <View style={globalStyles.container}>
+    <FlatList data={reviews} renderItem={({ item }) => (
+      <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+        <Text style={globalStyles.titleText}>{ item.title }</Text>
+      </TouchableOpacity>
+    )} />
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
