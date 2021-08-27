86b9eddad841d32c5c9f4c804f090bdf17508269
Vinay T S <vinaytshetty@gmail.com>
2021-08-27 15:09:45 +0530

    Added Different Demo Screens. Made to show Home screens.

--- ReactTutorials/App.js
+++ ReactTutorials/App.js
@@ -1,42 +1,7 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import HomeHeader from './headerscreen/HomeHeader';
-
+import React from 'react';
+import Home from './screens/home'
 export default function App() {
-  const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1' },
-    { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
-  ]);
-
-
   return (
-    <View style={styles.container}>
-      <HomeHeader />
-      <View style={styles.content}>
-        {/* add todo form */}
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <Text>{item.text}</Text>
-            )}
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
+<Home></Home>)
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    padding: 30,
-  },
-  list: {
-    marginTop: 0,
-  },
-});
