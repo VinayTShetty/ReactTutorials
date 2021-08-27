31543d8710b9dc0d7152f7c8f0ec8e5a228ee707
Vinay T S <vinaytshetty@gmail.com>
2021-08-27 16:35:12 +0530

    npm install @react-navigation/native expo install react-native-screens react-native-safe-area-context

--- ReactTutorials/App.js
+++ ReactTutorials/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './screens/home'
 export default function App() {
   return (
-<Home></Home>)
-};
+    <Home/>
+  );
+}
 
--- ReactTutorials/screens/home.js
+++ ReactTutorials/screens/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={globalStyles.container}>
+      <Text>Home Demo Screen</Text>
     </View>
   );
 }
@@ -14,3 +15,8 @@ const styles = StyleSheet.create({
     padding: 24,
   },
 });
+
+
+
+// https://snack.expo.dev/@guruparan/navsample
+//https://stackoverflow.com/questions/62761253/how-to-use-stacknavigator-with-drawernavigator-reactnavigation-v5-im-using-c
