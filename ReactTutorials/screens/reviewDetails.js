923bf2af7192abfced15b5f48e25b40f5960c8c8
Vinay T S <vinaytshetty@gmail.com>
2021-08-27 20:36:53 +0530

    image is not showing in the Review DetailsScreen. Checking it.

--- ReactTutorials/screens/reviewDetails.js
+++ ReactTutorials/screens/reviewDetails.js
@@ -4,14 +4,101 @@ import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails(props) {
   return (
-    <View style={globalStyles.container}>
-    <Image source={props.Image}/>
+    <View style={styles.container}>
+    <Image 
+    style={styles.userImage}
+    source={props.Image}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 24,
+  container:{
+    flex:5,
+    marginTop:20,
   },
-});
+  list: {
+    paddingHorizontal: 5,
+    backgroundColor:"#E6E6E6",
+  },
+  listContainer:{
+   alignItems:'center'
+  },
+  /******** card **************/
+  card:{
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 900,
+      height: 10,
+    },
+    shadowOpacity: 0.90,
+    shadowRadius: 9.49,
+    elevation: 20,
+
+    marginVertical: 25,
+    backgroundColor:"white",
+    flexBasis: '96%',
+    marginHorizontal: 5,
+  },
+  cardFooter: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: 'row',
+    alignItems:"center", 
+    justifyContent:"center"
+  },
+  cardContent: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+  },
+  cardHeader:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12.5,
+    paddingBottom: 25,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+  },
+  userImage:{
+    height: 150,
+    width: 300,
+ 
+    alignSelf:'center',
+    borderColor:"#DCDCDC",
+    borderWidth:3,
+  },
+  name:{
+    fontSize:18,
+    flex:1,
+    alignSelf:'center',
+    color:"#008080",
+    fontWeight:'bold'
+  },
+  position:{
+    fontSize:14,
+    flex:1,
+    alignSelf:'center',
+    color:"#696969"
+  },
+  followButton: {
+    marginTop:10,
+    height:35,
+    width:100,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+  followButtonText:{
+    color: "#FFFFFF",
+    fontSize:20,
+  },
+  icon:{
+    height: 20,
+    width: 20, 
+  }
+});   
